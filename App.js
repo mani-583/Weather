79742a5c22e2b19a66515db5989e6736738f41d9
mani-583 <101265102+mani-583@users.noreply.github.com>
2024-06-03 10:33:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import WeatherApp from './Component/WeatherApp/Weatherapp';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
